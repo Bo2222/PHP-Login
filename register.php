f696f8283844bb47735d1a85074a093c4b706864
Bo Lin <learldu91@gmail.com>
2025-03-04 12:09:01 +0800

    修改phpmyadmin的密碼

--- register.php
+++ register.php
@@ -1,7 +1,7 @@
 <?php
 $servername = "localhost";
 $username = "root";
-$password = "password";
+$password = "";
 $dbname = "user";
 
 #conn = new mysqli($servername, $username, $password, $dbname);
