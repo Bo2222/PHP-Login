b3bb6a3602615429aaa769b2b406ce2a97845241
Bo Lin <learldu91@gmail.com>
2025-03-04 10:53:20 +0800

    建立user資料表和註冊畫面文件(index.php)

---
+++ index.php
@@ -0,0 +1,13 @@
+<html>
+<head>
+    <title>註冊</title>
+</head>
+<body>
+    <h2>註冊新帳號</h2>
+    <form action = "register.php" method = "POST">
+        帳號：<input type = "text" name = "username" required><br>
+        密碼：<input type = "password" name = "password" required><br>
+        <input type = "submit" value = "註冊">
+    </form>
+</body>
+</html>
