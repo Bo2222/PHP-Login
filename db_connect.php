ade5c332a20c281254356ecbde719ce4a55c7735
Bo Lin <learldu91@gmail.com>
2025-03-04 15:23:06 +0800

    建立資料庫連接的處理檔案

---
+++ db_connect.php
@@ -0,0 +1,13 @@
+<?php
+$servername = "localhost";
+$username = "root";
+$password = "";
+$dbname = "user";
+
+$conn = new mysqli($servername, $username, $password, $dbname);
+
+if($conn->connect_error){
+    die("連接資料庫失敗:" . $conn->connect_error);
+}
+
+?>
