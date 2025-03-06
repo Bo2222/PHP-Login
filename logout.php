bf8f2069e4a5b2780e978f26d1bebd72ea318ccc
Bo Lin <learldu91@gmail.com>
2025-03-06 14:07:21 +0800

    完成登出功能的php

---
+++ logout.php
@@ -0,0 +1,22 @@
+<?php
+session_start(); // 啟動 Session
+
+// 清除所有 Session 變數
+$_SESSION = array();
+
+// 如果使用了 Cookies 存儲 Session，則銷毀 Cookie
+if (ini_get("session.use_cookies")) {
+    $params = session_get_cookie_params();
+    setcookie(session_name(), '', time() - 42000,
+        $params["path"], $params["domain"],
+        $params["secure"], $params["httponly"]
+    );
+}
+
+// 銷毀 Session
+session_destroy();
+
+// 跳轉到登入頁面
+header("Location: index.php");
+exit();
+?>
