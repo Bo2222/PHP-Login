83b2a28511dff31f97a4fb40015bc41a466419b6
Bo Lin <learldu91@gmail.com>
2025-03-06 14:06:27 +0800

    新增首頁的導航列和登入登出會員功能

--- index.php
+++ index.php
@@ -1,13 +1,42 @@
 <html>
 <head>
-    <title>註冊</title>
+    <title>網站導航</title>
+    <style>
+        nav {
+            background-color: #333;
+            padding: 10px;
+        }
+        nav ul {
+            list-style: none;
+            margin: 0;
+            padding: 0;
+            display: flex;
+        }
+        nav ul li {
+            margin-right: 20px;
+        }
+        nav ul li a {
+            color: white;
+            text-decoration: none;
+        }
+    </style>
 </head>
 <body>
-    <h2>註冊新帳號</h2>
-    <form action = "register.php" method = "POST">
-        帳號：<input type = "text" name = "username" required><br>
-        密碼：<input type = "password" name = "password" required><br>
-        <input type = "submit" value = "註冊">
-    </form>
+    <h2>網頁導覽</h2>
+    <!--<ul>
+        <li><a href="loginWeb.php">登入會員</a></li>
+        <li><a href="registerWeb.php">註冊會員</a></li>
+        <li><a href="orderWeb.php">預約項目</a></li>
+        <li><a href="orderView.php">訂單瀏覽</a></li>
+    </ul>-->
+    <nav>
+        <ul>
+            <li><a href="index.php">首頁</a></li>
+            <li><a href="loginWeb.php">登入會員</a></li>
+            <li><a href="logout.php">登出會員</a></li>
+            <li><a href="orderWeb.php">預約項目</a></li>
+            <li><a href="orderview.php">訂單瀏覽</a></li>
+        </ul>
+    </nav>
 </body>
 </html>
